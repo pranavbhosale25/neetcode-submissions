e1f0c2ff22babaaccd80e6736c1440190539374d
Pranav Bhosale <pranavbhosale25@gmail.com>
2026-08-10 20:09:27 -0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count freqs
+        # reverse sort by frequencies
+        # keep going until k met 
+        # and put in a list
+        # return list 
+
+        freqs = {}
+
+        for i in nums:
+            if i in freqs:
+                freqs[i] += 1
+            else: 
+                freqs[i] = 1
+        
+        reverseSortedFreqs = dict(sorted(freqs.items(), key=lambda item : item[1], reverse=True))
+
+        sol = []
+        for key in reverseSortedFreqs.keys():
+            sol.append(key)
+            if len(sol) == k:
+                break
+
+        return sol
+
+
+        
