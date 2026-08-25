a1fef4d52da433413e5bb01ac371d2e4ff640d5f
Pranav Bhosale <pranavbhosale25@gmail.com>
2026-08-24 22:57:43 -0400

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    calcs = {}
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2: 
+            return 2
+        
+        p1 = 2
+        p2 = 1
+        c = 0
+        for i in range(3,n+1):
+            c = p1 + p2
+            p2 = p1
+            p1 = c 
+
+        return p1
+        
